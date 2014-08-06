b040b1e94a1aec86d32067a2073103f5ab581e3a
Alain Leon <xerebz@gmail.com>
2014-08-06 15:49:22 -0400

    Init

---
+++ assets/javascripts/popover.js
@@ -0,0 +1,7 @@
+$().ready({
+  $('a').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+        text: 'My common piece of text here'
+    }
+  });
+});
---
+++ plugin.rb
@@ -0,0 +1,27 @@
+# name: emoji
+# about: emoji support for Discourse
+# version: 0.1
+# authors: Sam Saffron, Robin Ward
+
+register_asset('javascripts/skye.js.erb', :server_side)
+register_asset('stylesheets/skye.css')
+register_asset('javascripts/jquery.qtip.min.js')
+register_asset('stylesheets/jquery.qtip.min.css')
+register_asset('javascripts/popover.js')
+
+after_initialize do
+
+#  module SkyeItems
+#    class Engine < ::Rails::Engine
+#      engine_name "skye_popover"
+#      isolate_namespace SkyePopover
+#    end
+#    
+#    class SkyePopoverPluginController < ActionController::Base
+#    end
+#  end
+
+  # whitelist emojis so that new user can post emojis
+  Post::white_listed_image_classes << "skye"
+
+end
